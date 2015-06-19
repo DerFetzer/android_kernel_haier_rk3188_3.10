93e77fca42284a7ebb7d32d5004add15896763e1
Firefly <service@t-firefly.com>
2015-06-19 19:26:30 +0800

    [kernel] usb: bc: add battery charger detect head file
    Signed-off-by: Firefly <service@t-firefly.com>
    (cherry picked from commit beba843dc21b9096df5f4be77ed5622fb6b4b4f6)
    
    Conflicts:
    	drivers/usb/dwc_otg_310/usbdev_bc.h
    
    Signed-off-by: Firefly <service@t-firefly.com>

--- drivers/usb/dwc_otg_310/usbdev_bc.h
+++ drivers/usb/dwc_otg_310/usbdev_bc.h
@@ -1,15 +1,7 @@
 #ifndef _USBDEV_BC_H
 #define _USBDEV_BC_H
 
-/* USB Charger Types */
-enum bc_port_type{
-	USB_BC_TYPE_DISCNT = 0,
-	USB_BC_TYPE_SDP,
-	USB_BC_TYPE_DCP,
-	USB_BC_TYPE_CDP,
-	USB_BC_TYPE_UNKNOW,
-	USB_BC_TYPE_MAX,
-};
+#include <linux/power/rk_usbbc.h>
 enum {
 	BC_BVALID = 0,
 	BC_IDDIG,
@@ -71,11 +63,7 @@ USB Port Type
 2 : DCP - charger
 3 : CDP - pc with big currect charge
 ***********************************/
-extern int dwc_otg_check_dpdm(bool wait);
 extern enum bc_port_type usb_battery_charger_detect(bool wait);
 extern void rk_battery_charger_detect_cb(int charger_type_new);
-extern int rk_bc_detect_register_notifier(struct notifier_block *nb,
-						  enum bc_port_type *type);
-extern int rk_bc_detect_unregister_notifier(struct notifier_block *nb);
 
 #endif
